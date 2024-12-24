8a52aa20e3273facee82118840d1bdef637273a9
fjmasis <102482854+fjmasis@users.noreply.github.com>
2024-12-23 19:34:01 -0600

    CARPETA MODELS creada con AgendaController y el metodo get

---
+++ FranciscoMasisAgenda/Controllers/AgendaController.cs
@@ -0,0 +1,27 @@
+﻿using FranciscoMasisAgenda.DataBase;
+using FranciscoMasisAgenda.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FranciscoMasisAgenda.Controllers
+{
+    [ApiController]
+    [Route("api/agendas")]
+    public class AgendaController : Controller
+    {
+      private readonly AppDBContext _dbContext;
+
+        public AgendaController(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //Metodos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Agenda>>> GetAgenda()
+        {
+
+            return await _dbContext.agendas.ToListAsync();
+        }
+    }
+}
--- FranciscoMasisAgenda/DataBase/AppDBContext.cs
+++ FranciscoMasisAgenda/DataBase/AppDBContext.cs
@@ -1,8 +1,18 @@
-﻿namespace FranciscoMasisAgenda.DataBase
+﻿using FranciscoMasisAgenda.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FranciscoMasisAgenda.DataBase
 
 {
     public class AppDBContext : DbContext 
     {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
 
+        public DbSet<Agenda> agendas { get; set; } //sirve para dar el comportamiento de los datos de la tabla 
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Agenda>().HasKey(x => x.IdContacto); // especifico a llave primaria
+        }
     }
 }
